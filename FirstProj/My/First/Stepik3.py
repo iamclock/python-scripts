993b303a12cf1ced0c1276cb94cb527ddc735b09
iamclock <double7@mail.ru>
2021-07-14 17:09:15 +0700

    preparing for third part

--- FirstProj/My/First/Stepik3.py
+++ FirstProj/My/First/Stepik3.py
@@ -4,7 +4,59 @@ Created on 4 июл. 2021 г.
 @author: iamclock
 '''
 
+def first():
+	
+	
+	
+	return 0
 
+def temp():
+	return 0
+
+def temp1():
+	return 0
+
+def temp2():
+	return 0
+
+def temp3():
+	return 0
+
+def temp4():
+	return 0
+
+def temp5():
+	return 0
+
+def temp6():
+	return 0
+
+def temp7():
+	return 0
+
+def temp8():
+	return 0
+
+def temp9():
+	return 0
+
+def temp10():
+	return 0
+
+def temp11():
+	return 0
+
+def temp12():
+	return 0
+
+def temp13():
+	return 0
+
+def temp14():
+	return 0
+
+def temp15():
+	return 0
 
 
 if __name__ == '__main__':
