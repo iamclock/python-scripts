be6b6bcf7d54f34be2df8171f96bcfdaf10c01b5
iamclock <double7@mail.ru>
2021-07-15 21:03:57 +0700

    added 2 solutions for 2 tasks

--- FirstProj/My/First/Stepik3.py
+++ FirstProj/My/First/Stepik3.py
@@ -3,6 +3,9 @@ Created on 4 июл. 2021 г.
 
 @author: iamclock
 '''
+from My.First.Stepik import second
+
+# 	(x if x % 2 else None for x in items)
 
 '''
 Удаление нечётных элементов из списка, изменение чётных делением в два раза
@@ -49,11 +52,100 @@ def first_alt4(l):
 def first_alt5(l):
 	return 0
 
-def temp1():
+# def update_dictionary(d, key, value):
+def second():
+	d = {}
+	key = 1
+	value = -1
+	k2, k_or_k2 = 2*key, 0
+	for x in d:
+		if x == key: k_or_k2 |= 1
+		if x == k2: k_or_k2 |= 2
+	if k_or_k2&1: d[key] += [value]
+	elif k_or_k2&2: d[k2] += [value]
+	else: d[k2] = [value]
 	return 0
 
-def temp2():
+def second_alt1():
+	d = {}
+	key = 1
+	value = -1
+	key <<= key not in d
+	d[key] = d.get(key, []) + [value]
 	return 0
+
+def second_alt2():
+	d = {}
+	key = 1
+	value = -1
+	try: 
+		d[key].append(value)
+	except:
+		d.setdefault(key*2,[]).append(value)
+	return 0
+
+def second_alt3():
+	d = {}
+	key = 1
+	value = -1
+	if key in d:
+		d[key] = d[key] + [value]
+	else:
+		d[2 * key] = d.get(2 * key, []) + [value]
+	return 0
+
+'''
+a aa abC aa ac abc bcd a
+########################
+a A a
+'''
+def third():
+# 	words = [x.lower() for x in input().split()] # upper()
+	dic = {}
+	for word in input().lower().split():
+		dic[word] = dic.get(word) + 1 if dic.get(word) else 1
+	for key in dic:
+		print(key, dic[key])
+	return 0
+
+def third_alt1():
+	s = input().lower().split()
+	for i in set(s):
+		print(i, s.count(i))
+	return 0
+
+def third_alt2():
+	x = [i for i in input().lower().split()]
+	a = {i:x.count(i)for i in x}
+	for i,j in a.items():
+		print(i,j)
+	return 0
+
+def third_alt3():
+	text = input().lower().split()
+	d = { i : text.count(i) for i in text  }
+	for key, value in d.items():
+		print(key, value, end='\n')
+	return 0
+
+def third_alt4():
+	from collections import Counter
+	for i,j in Counter(input().lower().split(' ')).items():
+		print(i,j)
+	return 0
+
+def third_alt5():
+	w = input().lower().split()
+	print(*['%s %s' %(x, w.count(x)) for x in set(w)], sep='\n')
+	return 0
+
+def third_alt6():
+	l = input().lower().split()
+	d  = {e:0 for e in l}
+	for key in l: d[key]+= 1
+	for key,value in d.items(): print(key, value)
+	return 0
+
 
 def temp3():
 	return 0
@@ -96,6 +188,9 @@ def temp15():
 
 
 if __name__ == '__main__':
-	first()
+# 	first()
+# 	second()
+	third()
+	
 	
 	pass
