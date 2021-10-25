c3beb73e929dbd92f1f05be345ece498699c1231
iamclock <double7@mail.ru>
2021-10-25 22:51:57 +0700

    добавил комментарии в РСЛОС и исправил названия переменных

--- FirstProj/My/lfsr.py
+++ FirstProj/My/lfsr.py
@@ -3,41 +3,43 @@
 
 
 
-def LFSR(reg, poly):
-	bit = reg & 1
-	reg >>= 1
-	if bit:
-		reg = reg ^ poly
-	return reg, bit
+def lfsr(register, polynom):
+    '''
+    регистр сдвига с линейной обратной связью
+    register - текущее состояние регистра
+    polynom - примитивный многочлен
+    '''
+    bit = register & 1
+    register >>= 1
+    if bit:
+        register = register ^ polynom
+    return register, bit
 
 
 def chk_period():
-	#seed = reg = 0x3fffffff
-	#poly = 0x200004E1
-	#seed = reg = 0xace1
-	#poly = 0xb400
-	#seed = reg = 0xaffffffff
-	#poly = 0x4000006E3
-	#seed = reg = 0xffffffff
-	#poly = 0x80000A92
-	seed = reg = 0x1
-	poly = 0x5
-	
-	period = 0
-	print(str(hex(reg)))
-	while True:
-		reg, bit = LFSR(reg, poly)
-		print(bit, end="")
-		#print(str(hex(reg)))
-		period += 1
-		if reg == seed:
-			break
-	print()
-	print(str(hex(reg)))
-	print(str(period))
-	
-	
-
+    #seed = reg = 0x3fffffff
+    #poly = 0x200004E1
+    #seed = reg = 0xace1
+    #poly = 0xb400
+    #seed = reg = 0xaffffffff
+    #poly = 0x4000006E3
+    #seed = reg = 0xffffffff
+    #poly = 0x80000A92
+    seed = reg = 0x1
+    poly = 0x5
+    
+    period = 0
+    print(str(hex(reg)))
+    while True:
+        reg, bit = lfsr(reg, poly)
+        print(bit, end="")
+        #print(str(hex(reg)))
+        period += 1
+        if reg == seed:
+            break
+    print()
+    print(str(hex(reg)))
+    print(str(period))
 
 
 #def main():
