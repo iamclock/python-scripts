74d628ce70e20ca2b1d3cc52caaaf9680a23e3ea
iamclock <double7@mail.ru>
2021-11-02 22:41:55 +0700

    added script for looped running some stuff

--- FirstProj/My/run_win_com_loop.py
+++ FirstProj/My/run_win_com_loop.py
@@ -1,16 +1,38 @@
 '''
-Created on 10 окт. 2021 г.
+Created on 25 окт. 2021 г.
 
 @author: iamclock
 '''
 # -*- coding: utf-8 -*-
 import os
 from subprocess import Popen, PIPE 
-from string import ascii_letters, digits
+from string import ascii_lowercase, digits
 from random import choice, seed
+from inspect import stack
+import logging
+import re
 
+
+logFile = "container.log"
+logging.basicConfig(level=logging.INFO, filename=logFile, format='%(asctime)s %(levelname)s:%(message)s')
 containers = []
-templateSymbols = ascii_letters + digits
+templateSymbols = ascii_lowercase + digits
+suffixes = [
+    # "1",
+    # "2",
+    "3",
+    "4"
+    ]
+options = dict(zip(suffixes, [
+    # "\"1\"",
+    # "\"2\"",
+    "\"3\"",
+    "\"4\""
+    ]))
+keysCount = 1000
+
+def whoami():
+    return stack()[1][3]
 
 def lfsr(register, polynom):
     '''
@@ -24,93 +46,132 @@ def lfsr(register, polynom):
         register = register ^ polynom
     return register, bit
 
+def enum():
+    comm = f"\"enum"
+    output, _ = execute(comm)
+    return output
+
+def genKey(containerName, opts, is_print=False):
+    '''
+    Генерация ключа
+    '''
+    comm = f"\"gen --name {containerName} --alg {opts} --name {containerName}"
+    output, _ = execute(comm)
+    if is_print:
+        print("\n", comm)
+        print(output, "\n")
+    assert re.search(r".*Success.*", output), f"[{whoami()}] {comm} : Not success : {output}."
+    return output
+
+def rmContainer(containerName, is_print=False):
+    comm = f"\"del -n {containerName}"
+    output, _ = execute(comm)
+    if is_print:
+        print("\n", comm)
+        print(output, "\n")
+    assert re.search(r".*Success.*", output), f"[{whoami()}] {comm} : Not success : {output}."
+    return output
+
+def checkKey(keysList, containerName):
+    '''
+    Проверка, что ключ сгенерировался, контейнер создался
+    '''
+    assert containerName in keysList, f"[{whoami()}] : container {containerName} is not in the list"
+    logging.debug(f"[{whoami()}] : container {containerName} is in the list")
+    # print("checkKey: Passed")
+    return
+
 def execute(command):
     res = Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
-    output = res.stdout.read().decode('utf-8')
-    output += res.stderr.read().decode('utf-8')
+    output = res.stdout.read() # .decode('utf-8').strip()
+    output += res.stderr.read() #.decode('utf-8').strip()
+    output = output.decode('utf-8').strip()
     return output, res.returncode
 
-def genKey(containerName):
-    suffixes = ["_256", "_512"]
-    options = dict(zip(suffixes, [["", ""]]))
+def genAllKeys(containerName):
+    '''
+    Генерация ключей
+    '''
     for suffix in suffixes:
         curContName = f"{containerName}{suffix}"
-        comm = f" {curContName} {options[suffix]} {curContName} "
-        print(comm)
-        # output, retCode = execute(comm)
-        # if retCode > 0:
-        #     print(output)
-        #     raise ValueError("genKey suffix[1:] key: Return code doesn't equals 0")
+        try:
+            _ = genKey(curContName, options[suffix], is_print=False)
+        except AssertionError as er:
+            logging.error(er)
+            print("[ERROR]", er)
     return
 
-def checkKey(containerName):
-    suffixes = ["_256", "_512"]
-    comm = f""
-    output, retCode = execute(comm)
-    if retCode == 0:
-        for suffix in suffixes:
-            curContName = f"{containerName}{suffix}"
-            if not curContName in output:
-                raise ValueError(f"checkKey: container {curContName} is not in the list")
-            else:
-#                 print(f"checkKey: container {curContainerName} is in the list")
-                pass
-    else:
-        print(output)
-        raise ValueError("checkKey: Return code doesn't equals 0")
+def checkAllKeys(containerName):
+    '''
+    Проверка, что все ключи сгенерировались, контейнеры создались
+    '''
+    keysList = enum()
+    for suffix in suffixes:
+        curContName = f"{containerName}{suffix}"
+        try:
+            checkKey(keysList, curContName)
+        except AssertionError as er:
+            logging.error(f"{er}")
+            print("[ERROR]", er)
+            printEnum()
+    # print("checkKey: Passed")
     return
 
-def enum():
+def printEnum():
+    output = enum()
+    print(output)
+    # if 'Success' in output:
+    #     print("enum: Passed")
     return
 
-def remContainers():
-    suffixes = ["_256", "_512"]
+def removeContainers():
+    '''
+    Удаление контейнеров
+    '''
     while True:
         try:
             curContainerPrefix = containers.pop()
         except IndexError:
-            print("remContainers: No containers remaining")
+            logging.info(f"[{whoami()}]\t: No containers remaining")
             break
         for suffix in suffixes:
             curContainerName = f"{curContainerPrefix}{suffix}"
-#             comm = f" {curContainerName}"
-#             execute(comm)
-#             print(f"remContainers: container {curContainerName} successfully deleted")
+            try:
+                _ = rmContainer(curContainerName, is_print=False)
+            except AssertionError as er:
+                logging.error(er)
+                print("[ERROR]", er)
     return
 
 
 if __name__ == '__main__':
-#     path = "C:\Users\User"
+#     path = ""
 #     os.chdir(path)
     j = 0
     while True:
+        # rho3fe28r0r : 0xe8
+        # kyxmpced1as : 0xf2d
+        # 6Yp2f3uoslh : 0xb43
         lfsrSeed = 0x1
         lfsrMask = 0xb43
         lfsrCurState, _ = lfsr(lfsrSeed, lfsrMask)
         j += 1
+        startCleanUpCount = 200 # требуемое количество контейнеров для запуска очистки 
     #     i = 0
     #     while lfsrSeed != lfsrCurState:
     #         i += 1
-        for i in range(0, 501):
+        # lfsrErrorSeedList = [0xb43]
+        # for lfsrCurState in lfsrErrorSeedList:
+        for i in range(0, keysCount):
             seed(lfsrCurState)
-            newContainer = (''.join(choice(templateSymbols) for _ in range(10)))
+            newContainer = (''.join(choice(templateSymbols) for _ in range(11)))
             containers.append(newContainer)
-            """
-            try:
-                genKey(newContainer)
-                checkKey(newContainer)
-            except ValueError:
-                print("Wrong return code")
-                return
-            """
-    #         print(i, hex(lfsrCurState), sep='\t')
+            genAllKeys(newContainer)
+            checkAllKeys(newContainer)
             lfsrCurState, _ = lfsr(lfsrCurState, lfsrMask)
-#         print("        Containers:", )
-#         print(*containers, sep='\n', end="\n")
-#         print("        Remove containers:")
-        remContainers()
-#         print("        Containers:")
-#         print(*containers, sep='\n', end="\n")
-        print(j, i, sep="\t")
-#         break
-#     return
+            if not ((i + 1) % startCleanUpCount): # очистка списка созданных токенов каждые n раз
+                removeContainers()
+        logMsg = f"Iteration: {j}, Keys count: {(i+1)*2}"
+        logging.info(logMsg)
+        print(logMsg)
+        # break # for debuging purposes
