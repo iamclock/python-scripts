756636ae4979b3173973151ed51922827c07488c
iamclock <double7@mail.ru>
2021-11-02 22:40:32 +0700

    optimized algorithm

--- FirstProj/My/rename.py
+++ FirstProj/My/rename.py
@@ -2,15 +2,8 @@
 import os
 
 
-
-
-
-
-
-
 path = input("Enter path to folder: ")
 listOfFiles = os.listdir(path)
-countOfFiles = len(listOfFiles)
 os.chdir(path)
-for i in range(0, countOfFiles):
-    os.rename(listOfFiles[i], '0'+listOfFiles[i])
+for curFileName in listOfFiles:
+    os.rename(curFileName, '0'+curFileName)
